d571ad2af4e68ac68cfeff409c9b923bc054fbea
Camilodez <hernandezz_camilo@javeriana.edu.co>
2023-10-20 04:04:29 -0500

    comienzo del proyecto

---
+++ src/main/java/com/example/tallerprogreactiva/TallerProgReactivaApplication.java
@@ -0,0 +1,14 @@
+package com.example.tallerprogreactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TallerProgReactivaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TallerProgReactivaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tallerprogreactiva/TallerProgReactivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tallerprogreactiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerProgReactivaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
